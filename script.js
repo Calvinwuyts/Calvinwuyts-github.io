7f3d5eb9219405037ae117775fe5593ab8a1d2c1
Calvinwuyts <jolanwuyts@gmail.com>
2017-03-16 01:25:23 +0100

    installed youtube API styled genre and mood input text boxes created div for results todo: create hidden submit buttons, on submit request data from Youtube, cfr catstest.html

--- script.js
+++ script.js
@@ -21,9 +21,16 @@ function toggleNav() {
 function closeNav() {
     document.getElementById("mynavbar").style.width = "0";
 }
+
+/* initializing toggle variables telling us if the genre or mood buttons have been clicked */
 var genretoggle = false;
 var moodtoggle = false;
 
+/* a function that says if the genre button has been clicked, slide the genre lookup form down.
+If it is clicked again, slide it back up.
+If the mood toggle is true, then the mood lookup form has been slid down. Slide the mood form back up
+and slide the genre toggle down.
+ */
 function genrebutton(){
     if (genretoggle == false && moodtoggle == false){
         genretoggle = true;
@@ -38,6 +45,9 @@ function genrebutton(){
         $('#genre').slideToggle("slow");
     }
 }
+
+/* same story for the mood button.
+ */
 function moodbutton(){
     if (moodtoggle == false && genretoggle == false){
         moodtoggle = true;
@@ -51,4 +61,37 @@ function moodbutton(){
         moodtoggle = false;
         $('#mood').slideToggle("slow");
     }
+}
+
+/* submitting the genre and mood inputs on pressing enter
+
+
+
+$("#genreinput").keypress(function(event) {
+    if (event.which == 13) {
+        event.preventDefault();
+        genreresults();
+
+    }
+});
+
+$("#moodinput").keypress(function(event) {
+    if (event.which == 13) {
+        event.preventDefault();
+        $("#moodform").submit();
+    }
+});
+ */
+/* loading the Youtube API  */
+
+function init() {
+    gapi.client.setApiKey("AIzaSyDJgFr0e-Z5UF28f-klWqhPgS-k0efBFtU");
+    gapi.client.load("youtube", "v3", function () {
+    });
+    console.log('youtube API ready');
+}
+
+/* output the result of the genre form in the results div*/
+function genreresults(){
+    console.log("this executes.")
 }
